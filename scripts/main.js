24718108add912f20d9e5e38563934f0cb8bd477
Stephanie Souza <117867662+StephanieSouzaC@users.noreply.github.com>
2024-02-18 11:52:36 -0300

    updates

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,5 @@
 const swiper = new Swiper('.swiper', {
-    // speed: 400,
-    spaceBetween: 10,
+    spaceBetween: 15,
     slidesPerView: 1, /*Verificar como resolver isso de forma responsiva*/
     pagination: {
         el: '.swiper-pagination',
