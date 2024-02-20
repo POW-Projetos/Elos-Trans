92101c1d878db26a291d5a4932538ce0b93563dd
MarcosViniSil <Marcossilv203@gmail.com>
2024-02-20 19:16:27 -0300

    create section employees desktop

--- scripts/carousel.js
+++ scripts/carousel.js
@@ -1,5 +1,14 @@
-let currentIndex = 0;
 const totalItems = document.querySelectorAll('.section_team_cards_employee').length;
+let currentIndex = 0;
+
+window.addEventListener('resize', function () {
+    var width = window.innerWidth;
+    if (width >= 1000) {
+        currentIndex = 0
+        updateCarousel()
+    }
+});
+
 
 function updateCarousel() {
     document.querySelector('.carousel').style.transform = `translateX(${-currentIndex * 100}%)`;
