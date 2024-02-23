b3ace0f912eb25f2feb87ccf76cb3d5219e85708
MarcosViniSil <Marcossilv203@gmail.com>
2024-02-23 12:44:47 -0300

    created spinner

--- scripts/form.js
+++ scripts/form.js
@@ -1,21 +1,38 @@
 
 const scriptURL = 'https://script.google.com/macros/s/AKfycbyIublMQ3xBNFkb5tDF2Jclc1xAFDgMvndnZbY_4cqD7-hyrPpL1R0EMHeJPLr9Qq6w/exec'
 const form = document.forms['submitGoogleSheets']
+let isAvailable = true
+
 form.addEventListener('submit', e => {
-  e.preventDefault()
-  if (getSituation() != "false") {
-    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
-      .then(response => {
-        form.reset()
-        alreadySendData()
-        const button = document.getElementById('verify')
-        button.disabled = true;
-        button.innerHTML = "✔";
-      })
-      .catch(error => {
-        console.error('Error!', error.message)
-        alert("OPS!! Ocorreu um erro ao enviar o formulário, tente novamente")
-      })
+  const spinner = document.getElementById('loader')
+  const button = document.getElementById('verify')
+  if (isAvailable) {
+    
+    this.isAvailable = false
+    spinner.style.display = "block"
+    button.style.display = "none"
+
+    e.preventDefault()
+
+    if (getSituation() != "false") {
+      fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+        .then(response => {
+          form.reset()
+          spinner.style.display = "none"
+          button.style.display = "block"
+          alreadySendData()
+          button.disabled = true
+          button.innerHTML = "✔"
+        })
+
+        .catch(error => {
+          console.error('Error!', error.message)
+          alert("OPS!! Ocorreu um erro ao enviar o formulário, tente novamente")
+          spinner.style.display = "none"
+          button.style.display = "block"
+          this.isAvailable = true
+        })
+    }
   }
 })
 
